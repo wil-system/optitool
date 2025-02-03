1fc1e16afa49cc3d445878aaacf84a690b172420
lallafm1984 <lallafm1984@gmail.com>
2025-02-03 13:01:03 +0900

    02030100

--- app/api/operational-quantity/[id]/route.ts
+++ app/api/operational-quantity/[id]/route.ts
@@ -104,12 +104,11 @@ export async function GET(
   }
 }
 
-export async function DELETE(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function DELETE(request: Request)
+{
   try {
-    const { id } = params;
+    const { pathname } = new URL(request.url);
+    const id = pathname.split('/').pop();
     
     const { error } = await supabase
       .from('operational_quantities')
